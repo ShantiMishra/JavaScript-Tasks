4b4eedf5481bebde62d14b815e21099be29e11c5
ShantiMishra <shantimishra2006@gmail.com>
2025-12-03 23:25:34 +0530

    Text Input Display

---
+++ Task3_Text_Input_Display/app.js
@@ -0,0 +1,40 @@
+const exact = document.getElementById("normalText");
+const upperCase = document.getElementById("upperText");
+const lowerCase = document.getElementById("lowerText");
+const charCount = document.getElementById("charCount");
+
+const clearBtn = document.getElementById("clear-btn");
+const copyBtn = document.getElementById("copy-btn");
+const copyMsg = document.getElementById("copyMsg");
+
+const input = document.getElementById("userText");
+
+input.addEventListener("input" , ()=> {
+    const text = input.value ;
+
+    exact.textContent = "Exact : "+ text ;
+    upperCase.textContent = "Uppercase : " + text.toUpperCase();
+    lowerCase.textContent = "Lowercase : " + text.toLowerCase();
+    charCount.textContent = "Characters : "  + text.length();
+});
+
+clearBtn.addEventListener("click" , ()=>{
+    input.value = "";
+     exact.textContent = "Exact"+ text ;
+    upperCase.textContent = "Uppercase : ";
+    lowerCase.textContent = "Lowercase : " ;
+    charCount.textContent = "Character : 0" ;
+});
+
+copyBtn.addEventListener("click", function () {
+    const textToCopy = upperCase.textContent.replace("Uppercase: ", "");
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        copyMsg.classList.remove("hidden");
+
+        setTimeout(() => {
+            copyMsg.classList.add("hidden");
+        }, 2000);
+    });
+});
+
